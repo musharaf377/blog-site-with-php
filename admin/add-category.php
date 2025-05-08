b225888d5212ee747d83274742813089758ca967
musharaf-musemind <freelancer.musharaf@gmail.com>
2025-05-08 15:57:28 +0600

    Post Category Added.

--- admin/add-category.php
+++ admin/add-category.php
@@ -6,13 +6,38 @@
                   <h1 class="admin-heading">Add New Category</h1>
               </div>
               <div class="col-md-offset-3 col-md-6">
+            <?php
+                if(isset($_POST['submit'])){
+                    include 'config.php';
+
+                    $category_name = mysqli_real_escape_string($connection, $_POST['cat']);
+
+                    //select caetegory
+                    $select_query = "SELECT `category_name` FROM `category` WHERE `category_name`='$category_name'";
+                    $select_result = mysqli_query($connection, $select_query); 
+
+                    // print_r($select_query);
+
+                    if(mysqli_num_rows($select_result ) > 0){
+                        echo "Category name already Exist.";
+                    }else{
+                    $query = "INSERT INTO `category`(`category_name`) VALUES ('{$category_name}')";
+                        $result = mysqli_query($connection, $query);
+
+                        
+                    }
+
+                }
+            
+            ?>
+
                   <!-- Form Start -->
-                  <form action="" method="POST" autocomplete="off">
+                  <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
                       <div class="form-group">
                           <label>Category Name</label>
                           <input type="text" name="cat" class="form-control" placeholder="Category Name" required>
                       </div>
-                      <input type="submit" name="save" class="btn btn-primary" value="Save" required />
+                      <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
                   </form>
                   <!-- /Form End -->
               </div>
