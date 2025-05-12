dc35922b85bf5bf013ecbccf7c312cb21795438d
musharaf-musemind <freelancer.musharaf@gmail.com>
2025-05-12 18:16:57 +0600

    post add 50% complete

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,4 +1,8 @@
-<?php include "header.php"; ?>
+<?php 
+    include "header.php"; 
+    include "config.php";
+?>
+
   <div id="admin-content">
       <div class="container">
          <div class="row">
@@ -7,7 +11,7 @@
              </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form -->
-                  <form  action="" method="POST" enctype="multipart/form-data">
+                  <form  action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
                       <div class="form-group">
                           <label for="post_title">Title</label>
                           <input type="text" name="post_title" class="form-control" autocomplete="off" required>
@@ -18,8 +22,18 @@
                       </div>
                       <div class="form-group">
                           <label for="exampleInputPassword1">Category</label>
-                          <select name="category" class="form-control">
-                              <option value="" selected> Select Category</option>
+                          <select name="category" class="form-control" required>
+                              <option selected disabled> Select Category</option>
+                              <?php 
+                                $query = "SELECT * FROM `category`";
+                                $result = mysqli_query($connection, $query);
+                                
+                                if(mysqli_num_rows($result) > 0){
+                                    while($row = mysqli_fetch_assoc($result)){
+                                        echo "<option value='" . $row['category_id'] . "'>" . $row['category_name'] . "</option>";
+                                    }
+                                }
+                              ?>
                           </select>
                       </div>
                       <div class="form-group">
@@ -29,6 +43,42 @@
                       <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
                   </form>
                   <!--/Form -->
+
+                  <?php
+
+                  if(isset($_POST['submit'])){
+                    $title = mysqli_real_escape_string($connection, $_POST['post_title']);
+                    $postdesc = mysqli_real_escape_string($connection, $_POST['postdesc']);
+                    $category = $_POST['category'];
+                    $date = date("d M, Y");
+                    $author = $_SESSION['user_id'];
+                    $image = $_FILES['fileToUpload'];
+
+                    //image
+                    $file_error =[];
+
+                    $file_name = $image['name'];
+                    $file_size = $image['size'];
+                    $file_tmp = $image['tmp_name'];
+                    $file_type = $image['type'];
+                    $file_ext = end(explode('.', $file_name));
+
+                    $image_extention = ['jpeg','svg', 'png', 'jpg'];
+
+                    if(in_array($file_ext, $image_extention) == false){
+                        $file_error[] = "This extension file not allow.";
+                    }
+
+                    if($file_size > )
+                    print_r($file_ext);
+
+
+
+                    var_dump($image);
+                  }
+                  
+                  ?>
+
               </div>
           </div>
       </div>
